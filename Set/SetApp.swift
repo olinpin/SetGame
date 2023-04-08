94113e80fd13babf50be2728b07bf7502dc8f850
oliverhnat <oliver.hnat@gmail.com>
2023-04-08 13:38:12 +0100

    Initial Commit

---
+++ Set/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Set
+//
+//  Created by Oliver Hnát on 08.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
